d19d39bf07db6f3911d0779a6d7ef7d369539780
Slava2324 <113291519+Slava2324@users.noreply.github.com>
2024-11-13 20:02:08 +0300

    Added file

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+
+
+request_template = "https://wttr.in/{0}?m?n?q?T&lang=ru"
+for place in ["Лондон", "Аэропорт Шереметьево", "Череповец"]:
+	request = requests.get(request_template.format(place))
+	request.raise_for_status()
+	print(request.text)
