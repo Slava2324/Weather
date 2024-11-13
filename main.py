9c466052959c67738c03df6174ad244af29ba55a
Slava2324 <113291519+Slava2324@users.noreply.github.com>
2024-11-13 20:04:19 +0300

    Removed fatal bug

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from requests import get, raise_for_status
 
 request_template = "https://wttr.in/{0}?m?n?q?T&lang=ru"
 for place in ["Лондон", "Аэропорт Шереметьево", "Череповец"]:
-	request = requests.get(request_template.format(place))
+	request = get(request_template.format(place))
 	request.raise_for_status()
 	print(request.text)
