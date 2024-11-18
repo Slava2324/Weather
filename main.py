92eb782e972cc001508eaa34aeb62bb234893b63
Slava2324 <mirvec2010@gmail.com>
2024-11-18 19:20:38 +0300

    Updated main.py
    Description

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from requests import get
 
 
-request_template = "https://wttr.in/{0}?m?n?q?T&lang=ru"
-for place in ["Лондон", "Аэропорт Шереметьево", "Череповец"]:
-	request = get(request_template.format(place))
+get_parameters = {
+	"m": "",
+	"n": "",
+	"q": "",
+	"T": "",
+	"lang": "ru",
+}
+
+
+request_template = "https://wttr.in/{0}"
+for place in ["Лондон", "svo", "Череповец"]:
+	request = get(request_template.format(place), params=get_parameters)
 	request.raise_for_status()
 	print(request.text)
